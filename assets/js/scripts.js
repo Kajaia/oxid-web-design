4acd9e216a0295ffc38d95ed066031631c706ad6
Kajaia <lashakajaia31@gmail.com>
2024-10-13 00:09:48 +0400

    Added hero section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,29 @@
 "use strict";
+
+// AOS animation instantiation
+AOS.init({
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: "aos-init", // class applied after initialization
+  animatedClassName: "aos-animate", // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 500, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+});
+
+// Add shadow to navbar on scroll
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    document.querySelector("nav").classList.add("shadow");
+  } else {
+    document.querySelector("nav").classList.remove("shadow");
+  }
+});

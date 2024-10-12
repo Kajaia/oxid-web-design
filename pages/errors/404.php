c8a5f44349a5156026de60894a9b5020807da070
Kajaia <lashakajaia31@gmail.com>
2024-10-12 21:09:53 +0400

    Refactor to html

--- pages/errors/404.php
+++
@@ -1,9 +0,0 @@
-<?php include '../layout/header.php'; ?>
-<div class="col-12">
-    <h1>404 Not Found</h1>
-    <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora quis porro, mollitia expedita, incidunt nulla sunt ipsam provident asperiores voluptatem, saepe voluptas deserunt consequuntur! Ea esse omnis rerum repudiandae reprehenderit?
-    </p>
-    <a href="/">Return to home</a>
-</div>
-<?php include '../layout/footer.php'; ?>

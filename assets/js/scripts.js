587b26b04591d0ca9863f2d56d4d0c522030a282
Kajaia <lashakajaia31@gmail.com>
2024-10-15 16:45:34 +0400

    Added about section styling

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,7 +12,7 @@ AOS.init({
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
   offset: 120, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 500, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
   easing: "ease", // default easing for AOS animations
   once: true, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
